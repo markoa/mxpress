2969e6facd67b3709e942100041cfb6a5327d365
Marko Anastasov <marko@renderedtext.com>
2009-10-09 12:13:39 +0200

    Clean up for slightly smaller font size that displays the same in webkit

--- footer.php
+++ footer.php
@@ -11,14 +11,15 @@
 		<?php bloginfo('name'); ?> is written by
 		<a href="http://www.marko.anastasov.name" rel="me">Marko Anastasov</a>
 		and powered by <a href="http://wordpress.org/">WordPress</a>
-		<br /><a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a>
-		and <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.
+                with RSS feeds for
+                <a href="<?php bloginfo('rss2_url'); ?>">Entries</a>
+		and <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments</a>.
 		<!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
 	</p>
 </div>
 </div>
 
 <?php ?>
-		<?php wp_footer(); ?>
+		<!-- ?php wp_footer(); ?-->
 </body>
 </html>
