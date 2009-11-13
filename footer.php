2a20e91afc231f50e2187f19423e1ec15adfbfa6
Marko Anastasov <marko@renderedtext.com>
2009-11-13 13:29:07 +0100

    Add Google Analytics code

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package WordPress
- * @subpackage Default_Theme
+ * @subpackage mxpress
  */
 ?>
 
@@ -19,7 +19,16 @@
 </div>
 </div>
 
-<?php ?>
-		<!-- ?php wp_footer(); ?-->
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+	var pageTracker = _gat._getTracker("UA-11263928-1");
+	pageTracker._trackPageview();
+} catch(err) {}</script>
+
+  <!-- ?php wp_footer(); ?-->
 </body>
 </html>
